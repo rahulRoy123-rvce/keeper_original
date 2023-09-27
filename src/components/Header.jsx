e7a0304843bacce30456a4be2ef431c2e163f554
Rahul Roy <rahulroy.ec21@rvce.edu.in>
2023-09-28 00:29:57 +0530

    original

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      {/* <HighlightIcon /> */}
+      <h1>Today's Time Table</h1>
+    </header>
+  );
+}
+
+export default Header;
